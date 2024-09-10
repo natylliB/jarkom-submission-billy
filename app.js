3fded5393f05aa22a8eafc25b119a1ee83c0f857
BillyTan <www.billy39@gmail.com>
2024-09-10 13:48:26 +0700

    added route /me

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require('express')
 
 const PORT = 8000
-const HOST = '0.0.0.0'
+const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
 
 const app = express()
 /*
@@ -12,12 +12,15 @@ const app = express()
  */
 app.get('/', (req, res) => {
     res.send('Hello world!\n')
-})
+});
 
 /*
 Optional [ROUTE] '/me'
 [Response] => Dicoding username.
 */
+app.get('/me', (req, res) => {
+  res.send('dreamingpotato');
+});
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}`)
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
